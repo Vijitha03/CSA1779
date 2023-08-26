62f53659da36506f917fe905f47bd7b6ed36b3ef
Vijitha03 <113349410+Vijitha03@users.noreply.github.com>
2023-08-26 20:38:22 +0530

    Add files via upload

---
+++ 13.Minmax.py
@@ -0,0 +1,23 @@
+print("VIJITHA -192124187")
+print("Min Max Algorithm")
+import math
+def minimax (curDepth, nodeIndex,
+   maxTurn, scores,
+   targetDepth):
+ if (curDepth == targetDepth):
+  return scores[nodeIndex]
+ if (maxTurn):
+  return max(minimax(curDepth + 1, nodeIndex * 2,
+     False, scores, targetDepth),
+    minimax(curDepth + 1, nodeIndex * 2 + 1,
+     False, scores, targetDepth))
+ else:
+  return min(minimax(curDepth + 1, nodeIndex * 2,
+     True, scores, targetDepth),
+    minimax(curDepth + 1, nodeIndex * 2 + 1,
+     True, scores, targetDepth))
+scores = [13, 6, 12, 8, 10, 17, 2, 20]
+print(scores)
+treeDepth = math.log(len(scores), 2)
+print("The optimal value is : ", end = "")
+print(minimax(0, 0, True, scores, treeDepth))
